6ed84d0a9c437f70a96faf9686f3ba8d1aa30f76
simyoungdo04 <hodoouma53@gmail.com>
2026-05-15 17:48:56 +0900

    asdf

---
+++ SubwayProject/src/main/java/com/kh/order/controller/OrderInsertController.java
@@ -0,0 +1,26 @@
+package com.kh.order.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/order.do")
+public class OrderInsertController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public OrderInsertController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
